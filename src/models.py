0e3ce3dd44a940e815544396f922e89a2ef4ab75
Gilberto Diaz <gilberto.diaz@digitalreasoning.com>
2018-06-20 11:46:28 -0500

    post table

--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src import db
 
 
@@ -7,5 +9,19 @@ class User(db.Model):
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    # Adding the "author" attribute to the "Post"
+    # Dynamic creates a query instead of a list of posts
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
     def __repr__(self):
         return f"Username: {self.username}"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"<Post => {self.body}>"
