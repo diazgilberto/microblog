3a6efbbd07a1007d3bae89d3b15ed315d31e2f68
Gilberto Diaz <gilberto.diaz@digitalreasoning.com>
2018-06-20 14:58:50 -0500

    add loging and invalid login

--- microblog.py
+++ microblog.py
@@ -1,1 +1,11 @@
-from src import app
+from src.models import Post, User
+from src import app, db
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post,
+    }
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,7 @@
 from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, current_user
+
+from src.models import User
 from src import app
 from src.forms import LoginForm
 
@@ -16,8 +19,20 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash(f'Login requested by {form.username.data} with {form.remember_me.data}')
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user.username)
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Welcome back {user.username}')
+        return redirect(url_for('index'))
+
     return render_template('login.html', title='Sign In', form=form)
